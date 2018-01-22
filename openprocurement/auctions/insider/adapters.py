694429bdbfeb6ca68062e821909817229d1153aa
yarsanich <nezn27@gmail.com>
2018-01-22 16:25:36 +0200

    PEP

--- openprocurement/auctions/insider/adapters.py
+++ openprocurement/auctions/insider/adapters.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from openprocurement.auctions.core.adapters import AuctionConfigurator
 from openprocurement.auctions.insider.models import DGFInsider
-from openprocurement.auctions.core.plugins.awarding.v3.adapters import AwardingV3ConfiguratorMixin
+from openprocurement.auctions.core.adapters import AuctionConfigurator
+from openprocurement.auctions.core.plugins.awarding.v3.adapters import (
+    AwardingV3ConfiguratorMixin
+)
 
 
-class AuctionInsiderConfigurator(AuctionConfigurator, AwardingV3ConfiguratorMixin):
+class AuctionInsiderConfigurator(AuctionConfigurator,
+                                 AwardingV3ConfiguratorMixin):
     name = 'Auction Insider Configurator'
     model = DGFInsider
