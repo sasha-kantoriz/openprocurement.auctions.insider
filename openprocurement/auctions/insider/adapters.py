1e875f0dbbd2ae693f69d52434ef69d4c7dc654a
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-05-10 19:15:38 +0300

    Add insider AuctionManager

--- openprocurement/auctions/insider/adapters.py
+++ openprocurement/auctions/insider/adapters.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.insider.models import DGFInsider
-from openprocurement.auctions.core.adapters import AuctionConfigurator
+from openprocurement.auctions.core.adapters import (
+    AuctionConfigurator,
+    AuctionManagerAdapter
+)
 from openprocurement.auctions.core.plugins.awarding.v3.adapters import (
     AwardingV3ConfiguratorMixin
 )
@@ -10,3 +13,12 @@ class AuctionInsiderConfigurator(AuctionConfigurator,
                                  AwardingV3ConfiguratorMixin):
     name = 'Auction Insider Configurator'
     model = DGFInsider
+
+
+class AuctionInsiderManagerAdapter(AuctionManagerAdapter):
+
+    def create_auction(self, request):
+        pass
+
+    def change_auction(self, request):
+        pass
